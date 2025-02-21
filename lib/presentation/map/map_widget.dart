2582a56324073100ea84ab3134a8ba5ee0b74fec
Denis Schüle <schueledenis@gmx.at>
2025-02-21 22:00:31 +0100

    feat: add compass + map tile label

--- lib/presentation/map/map_widget.dart
+++ lib/presentation/map/map_widget.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_map/flutter_map.dart';
+import 'package:flutter_map_compass/flutter_map_compass.dart';
 import 'package:latlong2/latlong.dart';
 import 'package:map_bloc/application/location/location_cubit.dart';
 import 'package:map_bloc/domain/location/location_model_ext.dart';
@@ -22,14 +23,33 @@ class MapWidget extends StatelessWidget {
       options: MapOptions(
         initialCenter: locationState.userLocation.latLng(),
         initialZoom: 3.0,
+        interactionOptions: InteractionOptions(
+                  flags: InteractiveFlag.all & ~InteractiveFlag.rotate,
+                ),
+                maxZoom: 16,
       ),
       children: [
         TileLayer(
-          urlTemplate:
-              //'https://stamen-tiles.a.ssl.fastly.net/watercolor/{z}/{x}/{y}.png',
-              'https://tile.openstreetmap.org/{z}/{x}/{y}.png',
-          userAgentPackageName: 'com.example.map_tutorial',
+          urlTemplate: 'https://tile.openstreetmap.org/{z}/{x}/{y}.png',
+          tileBuilder: (context, tileWidget, tile) => Stack(
+            children: [
+              Opacity(opacity: 0.2, child: tileWidget),
+              Text(
+                '${tile.coordinates.z}/${tile.coordinates.x}/${tile.coordinates.y}',
+              ),
+            ],
+          ),
         ),
+        // );
+        // TileLayer(
+        //   urlTemplate:
+        //       //'https://stamen-tiles.a.ssl.fastly.net/watercolor/{z}/{x}/{y}.png',
+        //       'https://tile.openstreetmap.org/{z}/{x}/{y}.png',
+        //   userAgentPackageName: 'com.example.map_tutorial',
+          
+        // ),
+        const MapCompass.cupertino(),
+
         MarkerLayer(
           markers: [
             if (locationState.isUserLocationReady)
